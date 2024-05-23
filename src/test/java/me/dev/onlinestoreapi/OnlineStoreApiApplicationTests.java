fd7007632d31698c1808d44f6abca3b394660fe0
loftysky <lieuht2040@gmail.com>
2024-05-23 18:38:38 +0700

    Init

---
+++ src/test/java/me/dev/onlinestoreapi/OnlineStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dev.onlinestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
